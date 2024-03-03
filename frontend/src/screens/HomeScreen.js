1be1255a01be986d0abb82a7f093405aef8bad5d
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2024-03-03 17:13:21 +0530

    HomeScreen product Listing Component

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import products from '../products.js';
+import Products from '../components/Products.js';
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest Product</h1>
+            <Row>
+                {products.map((product) => (
+                    <Col sm={12} md={6} xl={3}>
+                        <Products product={product} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
