f7906351a648554d1850939fa5280dfb197debdc
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2024-03-05 16:38:16 +0530

    Costume Error Handling For Backend And Postman Api

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRouter from './routes/productRouter.js';
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 const app = express();
 dotenv.config();
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRouter)
 const PORT = process.env.PORT;
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server  Running in ${process.env.NODE_ENV} on  port ${PORT}`.brightYellow.bold)
